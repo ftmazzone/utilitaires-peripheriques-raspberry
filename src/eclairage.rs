8a90e0866800f63ec0cb58c082c9060271aa9311
Florent Mazzone <ftmazzone@yahoo.fr>
2022-12-18 16:58:45 +0100

    rajouter propriéta allume à Eclairage

--- src/eclairage.rs
+++ src/eclairage.rs
@@ -1,7 +1,8 @@
-use rppal::gpio::{Gpio,OutputPin};
+use rppal::gpio::{Gpio, OutputPin};
 
 pub struct Eclairage {
-      pin:  Option<OutputPin>,
+    pin: Option<OutputPin>,
+  pub  allume: bool,
 }
 
 impl Eclairage {
@@ -9,20 +10,29 @@ impl Eclairage {
         let gpio = Gpio::new().expect("Gpio new");
         let pin = gpio.get(pin).expect("gpio get").into_output();
         Self {
-            pin:Some(pin),
+            pin: Some(pin),
+            allume: false,
         }
     }
 
-    pub  fn demarrer(&mut self) {
+    pub fn demarrer(&mut self) {
         if self.pin.is_none() {
             return;
         }
         self.pin.as_mut().unwrap().set_high();
-        log::debug!("Eclairage allumé : {:?}",self.pin.as_mut().unwrap().is_set_high());
+        self.allume = true;
+        log::debug!(
+            "Eclairage allumé : {:?}",
+            self.pin.as_mut().unwrap().is_set_high()
+        );
     }
 
-    pub fn arreter( &mut self) {
-         self.pin.as_mut().unwrap().set_low();
-         log::debug!("Eclairage allumé : {:?}",self.pin.as_mut().unwrap().is_set_high());
+    pub fn arreter(&mut self) {
+        self.pin.as_mut().unwrap().set_low();
+        self.allume = false;
+        log::debug!(
+            "Eclairage allumé : {:?}",
+            self.pin.as_mut().unwrap().is_set_high()
+        );
     }
 }
