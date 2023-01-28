0a609290f764f4ecbd679e3aa70588fa1ddf91bc
Florent Mazzone <ftmazzone@yahoo.fr>
2023-01-28 19:35:18 +0100

    Tester initialisation

--- src/capteur_luminosite/capteur.rs
+++ src/capteur_luminosite/capteur.rs
@@ -30,24 +30,32 @@ impl Veml7700 {
         self.i2c
             .set_slave_address(AdresseCapteur::I2cAddress.adresse())?;
 
-        let gain = 0x00;//Instruction::AlsGain1_8.adresse();
-        let integration_time = 0x00;//Instruction::Als100MS.adresse();
+        let gain = 0x00; //Instruction::AlsGain1_8.adresse();
+        let integration_time = 0x00; //Instruction::Als100MS.adresse();
         let persistance = 0x00;
         let interrupt_enable = 0x00;
         let shutdown = 0x00;
 
-         let config_data:u16 = gain << 11
+        let config_data: u16 = gain << 11
             | integration_time << 6
             | persistance << 4
             | interrupt_enable << 1
             | shutdown << 0;
 
         let config_data = config_data.to_le_bytes();
-        println!("configuration {:?} big endian {}", config_data, self.big_endian);
+        println!(
+            "configuration {:?} big endian {}",
+            config_data, self.big_endian
+        );
 
         self.i2c
             .block_write(Instruction::AlsConfig as u8, &config_data)
             .unwrap();
+
+        println!(
+            "configuration {:?} big endian {}",
+            config_data, self.big_endian
+        );
 
         thread::sleep(Duration::from_secs(1));
 
