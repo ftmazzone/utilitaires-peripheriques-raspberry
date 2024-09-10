2a18a4962a0bf65ac18df0bdae931e5f4a1a9fb4
Florent Mazzone <ftmazzone@yahoo.fr>
2024-09-10 18:29:19 +0200

    renommer

--- exemple_rusttype/examples/afficher_jour_rusttype.rs
+++ exemple_rusttype/examples/afficher_jour_rusttype.rs
@@ -10,8 +10,8 @@ use std::{
 };
 
 use chrono::{Local, Locale, Timelike};
-use utilitaires::capteur_luminosite::capteur::Veml7700;
-use utilitaires::{detecteur::Detecteur, eclairage::Eclairage, ecran::ecran::Wepd7In5BV2};
+use utilitaires_peripheriques::capteur_luminosite::capteur::Veml7700;
+use utilitaires_peripheriques::{detecteur::Detecteur, eclairage::Eclairage, ecran::ecran::Wepd7In5BV2};
 use image::ImageBuffer;
 use log::log_enabled;
 use log::Level::Info;
