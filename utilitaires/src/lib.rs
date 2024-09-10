7ab344136bcf21abc64f63cf52e0cfc990e30b99
Florent Mazzone <ftmazzone@yahoo.fr>
2024-09-10 18:17:45 +0200

    Rendre les fonctionnalités optionnelles

--- utilitaires/src/lib.rs
+++ utilitaires/src/lib.rs
@@ -1,4 +1,8 @@
+#[cfg(feature = "ecran")]
 pub mod ecran;
+#[cfg(feature = "eclairage")]
 pub mod eclairage;
+#[cfg(feature = "detecteur")]
 pub mod detecteur;
+#[cfg(feature = "capteur_luminosite")]
 pub mod capteur_luminosite;
