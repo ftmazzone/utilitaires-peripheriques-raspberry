73fbab8e94c94168598b41613fbe8a19d8d75f34
Florent Mazzone <ftmazzone@yahoo.fr>
2024-11-07 21:43:05 +0100

    Rajouter un exemple pour lire la localisation

---
+++ exemple_utilitaire_peripheriques/examples/determiner_localisation.rs
@@ -0,0 +1,26 @@
+use std::{env, sync::{atomic::{AtomicBool, Ordering}, Arc}};
+
+use utilitaires_peripheriques::fournisseur_localisation;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+    
+    let arret_demande = Arc::new(AtomicBool::new(false));
+    let arret_demande_clone = arret_demande.clone();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        log::info!("Lecture des températures : arrêt demandé");
+        arret_demande_clone.store(true, Ordering::SeqCst);
+    });
+    let arret_demande_clone = arret_demande.clone();
+
+    let _ = fournisseur_localisation::verifier_localisation(
+        arret_demande_clone,
+        &Some("/sys/bus/usb/devices/1.1:1.0/1-1-port1".to_string()),
+    )
+    .await;
+    Ok(())
+}
